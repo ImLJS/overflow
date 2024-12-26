1b906b7ae2b3e3e1c1a8b600a1eb3a27380d7be9
ImLJS <109210868+ImLJS@users.noreply.github.com>
2024-12-26 19:58:38 +0530

    Update Left Sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,21 +1,22 @@
 import { ReactNode } from "react";
 
-import { auth } from "@/auth";
 import Navbar from "@/components/navigation/navbar";
 import LeftSidebar from "@/components/navigation/navbar/left-sidebar";
 
-const Rootlayout = async ({ children }: { children: ReactNode }) => {
-  const session = await auth();
-
+const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <main className="background-light850_dark100 relative">
+    <main className="background-light850_dark100 realtive">
       <Navbar />
+
       <div className="flex">
-        <LeftSidebar session={session} />
+        <LeftSidebar />
+
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl">{children}</div>
+        </section>
       </div>
-      {children}
     </main>
   );
 };
 
-export default Rootlayout;
+export default RootLayout;
