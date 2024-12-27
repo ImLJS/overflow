833572deeb4b5cbd4693104f978a8c4d594b2a9c
ImLJS <109210868+ImLJS@users.noreply.github.com>
2024-12-28 02:16:01 +0530

    Update Layout

--- components/navigation/navbar/right-sidebar.tsx
+++ components/navigation/navbar/right-sidebar.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 
 import TagCard from "@/components/cards/tag-card";
 import ROUTES from "@/constants/routes";
